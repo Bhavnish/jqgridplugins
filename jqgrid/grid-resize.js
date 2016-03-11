3994d7683551e305c39911e9596c963e4cd28deb
unknown <bbakshi@BBAKSHI-DNET.SEASIAINFOTECH.COM>
2016-03-11 13:01:22 +0530

    Initial commit

---
+++ jqgrid/grid-resize.js
@@ -0,0 +1,42 @@
+﻿var jqgridResize = {
+    resizejqGrid: function () {
+        $(".ui-jqgrid").each(function () {
+            var grid = $(this);
+            var nw = grid.parent().width();
+
+            if (!isNaN(nw)) {
+                var gridId = "#" + grid.attr("id").split('_')[1];
+                jQuery(gridId).jqGrid('setGridWidth', nw);
+            }
+        });
+
+
+    }
+};
+
+$(function () {
+    $(".ui-jqgrid").each(function () {
+        var grid = $(this);
+        var nw = grid.parent().width();
+        if (!isNaN(nw)) {
+            var gridId = "#" + grid.attr("id").split('_')[1];
+            jQuery(gridId).jqGrid('setGridWidth', nw);
+        }
+    });
+});
+  
+ 
+
+$(window).resize(function () {
+    try {
+        $(".ui-jqgrid").each(function () {
+            var grid = $(this);
+            var nw = grid.parent().width();
+            if (!isNaN(nw)) {
+                var gridId = "#" + grid.attr("id").split('_')[1];
+                jQuery(gridId).jqGrid('setGridWidth', nw);
+            }
+        });
+    } catch (e) {
+    }
+});
---
+++ jqgrid/gridColor.js
@@ -0,0 +1,21 @@
+﻿var gridColor = {
+    backgroundColor: '#ff0000',
+    left: '0px',
+    right: '0px',
+    bottom: '0px',
+    top: '0px',
+    height: '40px',
+    position: 'absolute',
+    object: '',
+
+    highlight: function () {
+        $('.grid-highlighter').remove();
+        gridColor.object.first().append('<span class="grid-highlighter" style="background-color:"' + gridColor.backgroundColor + '"; left:"' + gridColor.left + '"; bottom:"' + gridColor.bottom + '";height:"' + gridColor.height + '"; position: "' + gridColor.position + '"; top: "' + gridColor.top + '";">&nbsp;</span>');
+        //:eq( 2 )
+    }
+}
+
+$('tr').click(function () {
+    gridColor.object = $(this);
+    gridColor.highlight();
+});
